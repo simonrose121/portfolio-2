b573e7d006f156954bc30e4e124588f358973578
Simon Rose <simonrose121@gmail.com>
2020-09-19 12:31:52 +0100

    Install bootstrap and helmet

---
+++ src/html.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default function HTML(props) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        {props.headComponents}
+      </head>
+      <body>
+        <div id="___gatsby" dangerouslySetInnerHTML={{ __html: props.body }} />
+        {props.postBodyComponents}
+      </body>
+    </html>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,15 @@
 import React from "react"
+import Helmet from "react-helmet"
+
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import { Container } from "react-bootstrap"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <>
+      <Helmet title="Home" />
+      <Container>Hello world</Container>
+    </>
+  )
 }
